06869cbb3744f5aa420d211bdbfc8562ffa6d35e
feroult <feroult@gmail.com>
2018-09-18 18:17:55 -0300

    different environments for prod

--- src/main/java/br/com/dextra/dexboard/dao/NotificacaoDao.java
+++ src/main/java/br/com/dextra/dexboard/dao/NotificacaoDao.java
@@ -27,145 +27,155 @@ import br.com.dextra.dexboard.domain.RegistroAlteracao;
 
 public class NotificacaoDao {
 
-	private static final Logger LOG = LoggerFactory.getLogger(NotificacaoDao.class);
+    private static final Logger LOG = LoggerFactory.getLogger(NotificacaoDao.class);
 
-	private static final int DIAS_PARA_RENOTIFICAR = 3;
+    private static final int DIAS_PARA_RENOTIFICAR = 3;
 
-	private Objectify ofy;
+    private Objectify ofy;
 
-	public NotificacaoDao() {
-		RegisterClasses.register();
-		ofy = ObjectifyService.ofy();
-	}
+    public NotificacaoDao() {
+        RegisterClasses.register();
+        ofy = ObjectifyService.ofy();
+    }
 
-	public List<Projeto> buscarProjetosParaNotificar() {
-		List<Projeto> projetosAtrasados = buscarProjetosAtrasados();
+    public List<Projeto> buscarProjetosParaNotificar() {
+        List<Projeto> projetosAtrasados = buscarProjetosAtrasados();
 
-		List<Projeto> projetosParaNotificar = new ArrayList<Projeto>();
+        List<Projeto> projetosParaNotificar = new ArrayList<Projeto>();
 
-		for (Projeto projeto : projetosAtrasados) {
+        for (Projeto projeto : projetosAtrasados) {
 
-			Notificacao notificacao = buscarUltimaNotificacao(projeto);
+            Notificacao notificacao = buscarUltimaNotificacao(projeto);
 
-			if (notificacao == null || calculaDiasAtras(notificacao.getDate()) >= DIAS_PARA_RENOTIFICAR) {
-				projetosParaNotificar.add(projeto);
-			}
+            if (notificacao == null || calculaDiasAtras(notificacao.getDate()) >= DIAS_PARA_RENOTIFICAR) {
+                projetosParaNotificar.add(projeto);
+            }
 
-		}
+        }
 
-		return projetosParaNotificar;
-	}
+        return projetosParaNotificar;
+    }
 
-	private Notificacao buscarUltimaNotificacao(Projeto projeto) {
-		return ofy.load().type(Notificacao.class).id(projeto.getIdPma()).now();
-	}
+    private Notificacao buscarUltimaNotificacao(Projeto projeto) {
+        return ofy.load().type(Notificacao.class).id(projeto.getIdPma()).now();
+    }
 
-	private List<Projeto> buscarProjetosAtrasados() {
-		List<Projeto> projetosAtrasados = new ArrayList<Projeto>();
-		int validadeAlteracao = getValidadeAlteracao();
+    private List<Projeto> buscarProjetosAtrasados() {
+        List<Projeto> projetosAtrasados = new ArrayList<Projeto>();
+        int validadeAlteracao = getValidadeAlteracao();
 
-		List<Projeto> projetos = buscarProjetos();
-		for (Projeto projeto : projetos) {
-			List<Indicador> indicadores = buscarIndicadores(projeto);
-			for (Indicador indicador : indicadores) {
-				RegistroAlteracao registroAlteracao = buscarRegistrosAlteracoes(projeto, indicador);
+        List<Projeto> projetos = buscarProjetos();
+        for (Projeto projeto : projetos) {
+            List<Indicador> indicadores = buscarIndicadores(projeto);
+            for (Indicador indicador : indicadores) {
+                RegistroAlteracao registroAlteracao = buscarRegistrosAlteracoes(projeto, indicador);
 
-				if (registroAlteracao == null) {
-					projetosAtrasados.add(projeto);
-					break;
-				}
+                if (registroAlteracao == null) {
+                    projetosAtrasados.add(projeto);
+                    break;
+                }
 
-				Date dataAlteracao = registroAlteracao.getData();
-				long diasAtras = calculaDiasAtras(dataAlteracao);
+                Date dataAlteracao = registroAlteracao.getData();
+                long diasAtras = calculaDiasAtras(dataAlteracao);
 
-				if (diasAtras >= validadeAlteracao) {
-					projetosAtrasados.add(projeto);
-					break;
-				}
-			}
+                if (diasAtras >= validadeAlteracao) {
+                    projetosAtrasados.add(projeto);
+                    break;
+                }
+            }
 
-		}
-		return projetosAtrasados;
-	}
+        }
+        return projetosAtrasados;
+    }
 
-	private long calculaDiasAtras(Date data) {
-		return (new Date().getTime() - data.getTime()) / (1000 * 60 * 60 * 24);
-	}
+    private long calculaDiasAtras(Date data) {
+        return (new Date().getTime() - data.getTime()) / (1000 * 60 * 60 * 24);
+    }
 
-	private RegistroAlteracao buscarRegistrosAlteracoes(Projeto projeto, Indicador indicador) {
-		return ofy.load().type(RegistroAlteracao.class).filter("indicador", indicador).order("-data").first().now();
-	}
+    private RegistroAlteracao buscarRegistrosAlteracoes(Projeto projeto, Indicador indicador) {
+        return ofy.load().type(RegistroAlteracao.class).filter("indicador", indicador).order("-data").first().now();
+    }
 
-	private List<Indicador> buscarIndicadores(Projeto projeto) {
-		return ofy.load().type(Indicador.class).filter("projeto", projeto).list();
-	}
+    private List<Indicador> buscarIndicadores(Projeto projeto) {
+        return ofy.load().type(Indicador.class).filter("projeto", projeto).list();
+    }
 
-	private List<Projeto> buscarProjetos() {
-		return ofy.load().type(Projeto.class).filter("ativo", true).list();
-	}
+    private List<Projeto> buscarProjetos() {
+        return ofy.load().type(Projeto.class).filter("ativo", true).list();
+    }
 
-	public static int getValidadeAlteracao() {
-		String validadeProp = System.getProperty("validade");
-		if (validadeProp != null) {
-			return Integer.parseInt(validadeProp);
-		}
-		return 25;
-	}
+    public static int getValidadeAlteracao() {
+        String validadeProp = System.getProperty("validade");
+        if (validadeProp != null) {
+            return Integer.parseInt(validadeProp);
+        }
+        return 25;
+    }
 
-	public void notificarEquipeProjeto(Projeto projeto) {
-		enviarEmailEquipeProjeto(projeto);
-		registrarDataNotificacao(projeto);
-	}
+    public void notificarEquipeProjeto(Projeto projeto) {
+        enviarEmailEquipeProjeto(projeto);
+        registrarDataNotificacao(projeto);
+    }
 
-	private void enviarEmailEquipeProjeto(Projeto projeto) {
-		Properties props = new Properties();
-		Session session = Session.getDefaultInstance(props, null);
+    private void enviarEmailEquipeProjeto(Projeto projeto) {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
 
-		Message msg = new MimeMessage(session);
+        Message msg = new MimeMessage(session);
 
-		LOG.info("Enviando email de notificacao: projeto=" + projeto.getNome() + ", equipe=" + projeto.getEquipe() + ", email="
-				+ projeto.getEmail());
+        LOG.info("Enviando email de notificacao: projeto=" + projeto.getNome() + ", equipe=" + projeto.getEquipe() + ", email="
+                + projeto.getEmail());
 
-		try {
-			msg.setFrom(new InternetAddress("dexboard@dextra-sw.com", "Dexboard Reload"));
-			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(projeto.getEmail(), projeto.getEquipe()));
-			msg.addRecipient(Message.RecipientType.BCC, new InternetAddress("jose.guedes@dextra-sw.com", "Ze"));
-			msg.addRecipient(Message.RecipientType.BCC, new InternetAddress("fernando@dextra-sw.com", "Fernando"));
-			msg.setSubject("[dexboard] Atualizar projeto: " + projeto.getNome());
-			msg.setContent(createMessageBody(projeto), "text/html");
-			Transport.send(msg);
-		} catch (UnsupportedEncodingException | MessagingException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            msg.setFrom(new InternetAddress("dexboard@dextra-sw.com", "Dexboard Reload"));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(projeto.getEmail(), projeto.getEquipe()));
+            addBccs(msg);
+            msg.setSubject("[dexboard] Atualizar projeto: " + projeto.getNome());
+            msg.setContent(createMessageBody(projeto), "text/html");
+            Transport.send(msg);
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private String createMessageBody(Projeto projeto) {
-		StringBuilder msgBody = new StringBuilder();
-		msgBody.append(String.format("<p><b>%s</b>, n&atilde;o deixe seu projeto <b>%s</b> ficar desatualizado...</p>",
-				projeto.getEquipe(), projeto.getNome()));
-		msgBody.append("<p>Acesse o dexboard para atualiz&aacute;-lo: <a href=\"http://dexboard-reload.appspot.com\">dexboard-reload.appspot.com</a></p>");
-		msgBody.append("<img width=300 src=\"" + getImg() + "\"/>");
-		return msgBody.toString();
-	}
+    private void addBccs(Message msg) throws MessagingException, UnsupportedEncodingException {
+        String bccProperty = System.getProperty("dxb.bcc");
+        if (bccProperty == null) {
+            return;
+        }
+        String[] bccs = bccProperty.replaceAll("\\s+", "").split(",");
+        for (String bcc : bccs) {
+            msg.addRecipient(Message.RecipientType.BCC, new InternetAddress(bcc, bcc));
+        }
+    }
 
-	private String getImg() {
-		String[] imgs = { "https://bensbitterblog.files.wordpress.com/2013/05/brst-thing-on-a-lazy-saturday-77986722415.jpg?w=676",
-				"http://itsinfoworld.com/wp-content/uploads/2015/03/Being-lazy.jpg",
-				"http://www.hercampus.com/sites/default/files/2014/04/18/Our_lazy_cat_by_tariqphoto.jpg",
-				"https://academichelp.net/wp-content/uploads/2014/01/laziness.jpg",
-				"http://ecdn.funzypics.com/grumpycatmemes/pics/25/Laziness-Is-The-Mother-Of-Improvisation.jpg",
-				"https://farm8.staticflickr.com/7639/16830167905_fafc311f5a_o.jpg" };
-		return imgs[randomIndex(imgs)];
-	}
+    private String createMessageBody(Projeto projeto) {
+        StringBuilder msgBody = new StringBuilder();
+        msgBody.append(String.format("<p><b>%s</b>, n&atilde;o deixe seu projeto <b>%s</b> ficar desatualizado...</p>",
+                projeto.getEquipe(), projeto.getNome()));
+        msgBody.append("<p>Acesse o dexboard para atualiz&aacute;-lo: <a href=\"http://dexboard-reload.appspot.com\">dexboard-reload.appspot.com</a></p>");
+        msgBody.append("<img width=300 src=\"" + getImg() + "\"/>");
+        return msgBody.toString();
+    }
 
-	private int randomIndex(String[] imgs) {
-		return new Random().nextInt(imgs.length);
-	}
+    private String getImg() {
+        String[] imgs = {"https://bensbitterblog.files.wordpress.com/2013/05/brst-thing-on-a-lazy-saturday-77986722415.jpg?w=676",
+                "http://itsinfoworld.com/wp-content/uploads/2015/03/Being-lazy.jpg",
+                "http://www.hercampus.com/sites/default/files/2014/04/18/Our_lazy_cat_by_tariqphoto.jpg",
+                "https://academichelp.net/wp-content/uploads/2014/01/laziness.jpg",
+                "http://ecdn.funzypics.com/grumpycatmemes/pics/25/Laziness-Is-The-Mother-Of-Improvisation.jpg",
+                "https://farm8.staticflickr.com/7639/16830167905_fafc311f5a_o.jpg"};
+        return imgs[randomIndex(imgs)];
+    }
 
-	private void registrarDataNotificacao(Projeto projeto) {
-		Notificacao notificacao = new Notificacao();
-		notificacao.setDate(new Date());
-		notificacao.setIdPma(projeto.getIdPma());
-		ofy.save().entity(notificacao).now();
-	}
+    private int randomIndex(String[] imgs) {
+        return new Random().nextInt(imgs.length);
+    }
+
+    private void registrarDataNotificacao(Projeto projeto) {
+        Notificacao notificacao = new Notificacao();
+        notificacao.setDate(new Date());
+        notificacao.setIdPma(projeto.getIdPma());
+        ofy.save().entity(notificacao).now();
+    }
 }
--- src/main/java/br/com/dextra/dexboard/planilha/PlanilhaDexboard.java
+++ src/main/java/br/com/dextra/dexboard/planilha/PlanilhaDexboard.java
@@ -3,22 +3,22 @@ package br.com.dextra.dexboard.planilha;
 import br.com.dextra.dexboard.servlet.Context;
 
 class PlanilhaDexboard extends Planilha {
-	
-	// TODO: trazer de propriedades do sistema
 
-	private static final String CHAVE_PLANILHA_DEXBOARD_PRODUCAO = "1Fjul3zOetgENCTfnnd3gDaxOEh3WaIQr4uV3Th2wRQo";
+    // TODO: trazer de propriedades do sistema
 
-	private static final String CHAVE_PLANILHA_DEXBOARD_TESTE = "11z7PwH4sADaiXxshNPYr-88u3flqewCcGRGJok9cl6o";
+    private static final String CHAVE_PLANILHA_DEXBOARD_PRODUCAO = System.getProperty("dxb.spreadhseet");
 
-	public PlanilhaDexboard(String nomePlanilha) {
-		super(getChavePlanilha(), nomePlanilha);
-	}
+    private static final String CHAVE_PLANILHA_DEXBOARD_TESTE = "11z7PwH4sADaiXxshNPYr-88u3flqewCcGRGJok9cl6o";
 
-	private static String getChavePlanilha() {
-		if (Context.isIntegrationTestEnvironment()) {
-			return CHAVE_PLANILHA_DEXBOARD_TESTE;
-		}
-		return CHAVE_PLANILHA_DEXBOARD_PRODUCAO;
-	}
+    public PlanilhaDexboard(String nomePlanilha) {
+        super(getChavePlanilha(), nomePlanilha);
+    }
+
+    private static String getChavePlanilha() {
+        if (Context.isIntegrationTestEnvironment()) {
+            return CHAVE_PLANILHA_DEXBOARD_TESTE;
+        }
+        return CHAVE_PLANILHA_DEXBOARD_PRODUCAO;
+    }
 
 }
