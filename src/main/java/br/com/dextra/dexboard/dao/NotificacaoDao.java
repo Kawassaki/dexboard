606622499de451933395dfb5603a23d8ba7eaa38
feroult <feroult@gmail.com>
2018-09-20 10:35:04 -0300

    domain auth

--- src/main/java/br/com/dextra/dexboard/dao/NotificacaoDao.java
+++ src/main/java/br/com/dextra/dexboard/dao/NotificacaoDao.java
@@ -14,6 +14,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import br.com.dextra.dexboard.utils.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -139,10 +140,7 @@ public class NotificacaoDao {
     }
 
     private void addBccs(Message msg) throws MessagingException, UnsupportedEncodingException {
-        String bccProperty = System.getProperty("dxb.bcc");
-        if (bccProperty == null) {
-            return;
-        }
+        String bccProperty = Config.getProperty("dxb.bcc", "fernando@dextra-sw.com");
         String[] bccs = bccProperty.replaceAll("\\s+", "").split(",");
         for (String bcc : bccs) {
             msg.addRecipient(Message.RecipientType.BCC, new InternetAddress(bcc, bcc));
--- src/main/java/br/com/dextra/dexboard/servlet/AuthFilter.java
+++ src/main/java/br/com/dextra/dexboard/servlet/AuthFilter.java
@@ -12,82 +12,85 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.dextra.dexboard.utils.Config;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 public class AuthFilter implements Filter {
 
-	@Override
-	public void destroy() {
+    public static final String ORG_DOMAIN = Context.isProductionEnvironment() ? Config.getProperty("dxb.domain", "dextra-sw.com") : "dextra-sw.com";
 
-	}
+    @Override
+    public void destroy() {
 
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) resp;
+    }
 
-		UserService service = UserServiceFactory.getUserService();
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
 
-		String uri = request.getRequestURI();
-		User user = service.getCurrentUser();
+        UserService service = UserServiceFactory.getUserService();
 
-		if (isAdminPath(uri)) {
-			chain.doFilter(request, response);
-			return;
-		}
+        String uri = request.getRequestURI();
+        User user = service.getCurrentUser();
 
-		if (user == null) {
-			if ("GET".equals(request.getMethod())) {
-				goToLoginPage(response, service, uri);
-				return;
-			}
+        if (isAdminPath(uri)) {
+            chain.doFilter(request, response);
+            return;
+        }
 
-			unautorizedRequest(response, service);
-			return;
-		}
+        if (user == null) {
+            if ("GET".equals(request.getMethod())) {
+                goToLoginPage(response, service, uri);
+                return;
+            }
 
-		if (isLogoutPath(uri) || !isDextraUser(user)) {
-			goToLogoutPage(response, service);
-			return;
-		}
+            unautorizedRequest(response, service);
+            return;
+        }
 
-		chain.doFilter(request, response);
-		return;
-	}
+        if (isLogoutPath(uri) || !isValidOrgUser(user)) {
+            goToLogoutPage(response, service);
+            return;
+        }
 
-	private boolean isAdminPath(String uri) {
-		return uri.startsWith("/_ah") || uri.startsWith("/cron/") || uri.startsWith("/_tools");
-	}
+        chain.doFilter(request, response);
+        return;
+    }
 
-	private void unautorizedRequest(HttpServletResponse response, UserService service) throws IOException {
-		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
-		writer.append("<html><head><title>Murkfight</title></head><body>You need login <a href=\"");
-		writer.append(service.createLoginURL("/"));
-		writer.append("\">here</a></body></html>");
-	}
+    private boolean isAdminPath(String uri) {
+        return uri.startsWith("/_ah") || uri.startsWith("/cron/") || uri.startsWith("/_tools");
+    }
 
-	private void goToLoginPage(HttpServletResponse response, UserService service, String uri) throws IOException {
-		response.sendRedirect(service.createLoginURL(uri));
-	}
+    private void unautorizedRequest(HttpServletResponse response, UserService service) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.append("<html><head><title>Murkfight</title></head><body>You need login <a href=\"");
+        writer.append(service.createLoginURL("/"));
+        writer.append("\">here</a></body></html>");
+    }
 
-	private void goToLogoutPage(HttpServletResponse response, UserService service) throws IOException {
-		response.sendRedirect(service.createLogoutURL("/"));
-	}
+    private void goToLoginPage(HttpServletResponse response, UserService service, String uri) throws IOException {
+        response.sendRedirect(service.createLoginURL(uri));
+    }
 
-	private boolean isLogoutPath(String uri) {
-		return uri.equals("/logout");
-	}
+    private void goToLogoutPage(HttpServletResponse response, UserService service) throws IOException {
+        response.sendRedirect(service.createLogoutURL("/"));
+    }
 
-	private boolean isDextraUser(User user) {
-		return user.getEmail().endsWith("@dextra-sw.com");
-	}
+    private boolean isLogoutPath(String uri) {
+        return uri.equals("/logout");
+    }
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
+    private boolean isValidOrgUser(User user) {
+        return user.getEmail().endsWith("@" + ORG_DOMAIN);
+    }
 
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
 }
