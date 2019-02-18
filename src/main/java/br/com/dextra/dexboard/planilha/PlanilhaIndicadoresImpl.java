c19b5b66d23e49c6afb198f5fb1c3e0b2320ee1f
Elias Reis <eliasreis54@gmail.com>
2019-02-18 08:25:49 -0300

    Remove unnecessary lines

--- src/main/java/br/com/dextra/dexboard/planilha/PlanilhaIndicadoresImpl.java
+++ src/main/java/br/com/dextra/dexboard/planilha/PlanilhaIndicadoresImpl.java
@@ -15,15 +15,16 @@ class PlanilhaIndicadoresImpl extends PlanilhaDexboard implements PlanilhaIndica
 	private String buscarNomeDoIndicador(int linha) {
 		return recuperarConteudoCelula(linha, 1);
 	}
+
 	private String buscarDescricaoDoIndicador(int linha) {
 		return recuperarConteudoCelula(linha, 2);
 	}
 
 	@Override
 	public List<Indicador> criarListaDeIndicadores() {
-		List<Indicador> indicadores = new ArrayList<Indicador>();
+		List<Indicador> indicadores = new ArrayList<>();
 
-		Long i = 1l;
+		Long i = 1L;
 		while (true) {
 			String nomeIndicador = buscarNomeDoIndicador(i.intValue());
 			String descricao = buscarDescricaoDoIndicador(i.intValue());
