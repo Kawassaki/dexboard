67bc849fd93ef72f777e62e1d711137b89855eba
Elias Reis <eliasreis54@gmail.com>
2019-02-18 23:59:58 -0300

    Add project info box

--- src/main/java/br/com/dextra/dexboard/mock/MockPlanilhaIndicadores.java
+++ src/main/java/br/com/dextra/dexboard/mock/MockPlanilhaIndicadores.java
@@ -15,7 +15,9 @@ public class MockPlanilhaIndicadores implements PlanilhaIndicadores {
 				new Indicador(3L, "Foco em entrega do valor", "Descrição 3"),
 				new Indicador(4L, "Qualidade funcional", "Como está a visão da equipe sobre a qualidade técnica do projeto? O time tem orgulho do que está produzindo?"),
 				new Indicador(5L, "Qualidade do c\u00F3digo", "Como está a qualidade da experiência do usuário com o sistema?"),
-				new Indicador(6L, "CPI", "Avalia como está o custo do projeto, em relação ao orçamento previsto. O ideal é que o indicador seja maior ou igual a 1. Um número menor do que 1 indica que estamos gastando mais do que o previsto, como funciona com um testo grande")
+				new Indicador(6L, "CPI", "Avalia como está o custo do projeto, em relação ao orçamento previsto. O ideal é que o indicador seja maior ou igual a 1. Um número menor do que 1 indica que estamos gastando mais do que o previsto, como funciona com um testo grande"),
+				new Indicador(7L, "Indicador 7", "Descricao do indicador 7"),
+				new Indicador(8L, "Indicador 8", "Descricao do indicador 8")
 		);
 	}
 }
