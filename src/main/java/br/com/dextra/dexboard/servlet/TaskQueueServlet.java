beb1b35c1ed7a5ea04e0d086ed3c360415d15ce5
André Philippe Menegon <andrepmenegon@hotmail.com>
2019-02-27 15:27:24 -0300

    [Bug fix] Correção de time out.

---
+++ src/main/java/br/com/dextra/dexboard/servlet/TaskQueueServlet.java
@@ -0,0 +1,20 @@
+package br.com.dextra.dexboard.servlet;
+
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TaskQueueServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Queue queue = QueueFactory.getDefaultQueue();
+		queue.add(TaskOptions.Builder.withUrl("/cron/reload/projetos").method(TaskOptions.Method.GET));
+        resp.getWriter().print("Reload adicionado a fila.");
+    }
+
+}
